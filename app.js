a47954ca84603e57d12ca1c80bac51ccf6ac3047
Gordon Hennesy <gordonhennesy@gmail.com>
2020-10-17 12:47:18 -0700

    Added article edit and delete

--- app.js
+++ app.js
@@ -22,23 +22,35 @@ function insert_article(title, text) {
 //  console.log(articles);
   return;
 }
+function update_article(id, title, text) {
+  const db = require('better-sqlite3')('database.sqlite', []);
+  const sql = "UPDATE articles set title=?, text = ? WHERE id = ?";
+  console.log(sql);
+
+  db.prepare(sql).run(title, text, id);
+  return;
+}
 function get_article(artId) {
-  const sql = `SELECT title, text FROM articles
-           WHERE id = artId`;
   const db = require('better-sqlite3')('database.sqlite', []);
 
   const article = db.prepare('SELECT * FROM articles WHERE id = ?').get(artId);
   console.log(article);
   return article;
 }
+function delete_article(artId) {
+  const db = require('better-sqlite3')('database.sqlite', []);
+
+  db.prepare('DELETE FROM articles WHERE id = ?').run(artId);
+  return;
+}
 function get_articles() {
-  const sql = `SELECT DISTINCT title title, text FROM articles
-           ORDER BY title`;
   const db = require('better-sqlite3')('database.sqlite', []);
 
   const articles = db.prepare('SELECT * FROM articles').all();
   return articles;
 }
+
+// ROUTES
 app.get('/articles', function (req, res) {
   console.log('index articles');
   try {
@@ -60,32 +72,39 @@ app.get('/articles/create', function (req, res) {
 });
 app.post('/articles', function (req, res) {
   console.log('store new article')
-  //console.log(req);
-console.log(req.params);
-console.log(req.query);
-console.log(req.body);
+  console.log(req.body);
   insert_article(req.body.title, req.body.text);  
-  res.render('articles/store', { params: req.params, title: 'Articles', message: 'Hello there, single article lover!' })
+  res.redirect(301,'/articles');
+});
+function handle_patch_put(artId, title, text){
+  console.log(title);
+  article = update_article(artId, title, text);
+}
+app.post('/articles/:artId', function (req, res) {
+  handle_patch_put(req.params.artId, req.body.title, req.body.text);
+  res.redirect(301,'/articles');
+});
+app.put('/articles/:artId', function (req, res) {
+  console.log('put article #'+req.params.artId);
+  handle_patch_put(req.params.artId, req.body.title, req.body.text);
+  res.redirect(301,'/articles');
+});
+app.patch('/articles/:artId', function (req, res) {
+  console.log('patch article #'+req.params.artId);
+  handle_patch_put(req.params.artId, req.body.title, req.body.text);
+  res.redirect(301,'/articles');
 });
 app.get('/articles/:artId/edit', function (req, res) {
   console.log('edit article')
+  article = get_article(req.params.artId);
   res.render('articles/edit', { artid: req.params.artId, title: 'Articles', message: 'Edit article' })
 });
-app.patch('/articles/:artId', function (req, res) {
-  console.log('patch article #'+req.params.artId)
-  res.render('articles/edit', { artid: req.params.artId, title: 'Articles', message: 'Updating article #'+req.params.artId })
-});
 app.delete('/articles/:artId', function (req, res) {
-  console.log('destroy article')
-  res.render('articles/delete', { artid: req.params.artId, title: 'Articles', message: 'Hello there, single article lover!' })
+  console.log('destroy article'+req.params.artId);
+  delete_article(req.params.artId);
+  res.redirect(301,'/articles');
+  //res.render('articles/delete', { artid: req.params.artId, title: 'Articles', message: 'Hello there, single article lover!' })
 });
-app.get('/articles/:artId', function (req, res) {
-  console.log('show article #'+req.params.artId)
-  res.render('articles/index', { artid: req.params.artId, title: 'Articles', message: 'Hello there, single article lover!' })
-});
-//app.get('/articles', function (req, res) {
-//  res.render('articles/index', { title: 'Articles', message: 'Hello there, article lover!' })
-//});
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
